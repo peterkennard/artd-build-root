eb4634fc7193d3058429c54431ec960912aa5be2
peterk <peterk@livingwork.com>
2023-03-30 02:24:38 -0400

    added createShaderTasks to VulkanTools

--- build-scripts/rakish/VulkanTools.rb
+++ build-scripts/rakish/VulkanTools.rb
@@ -11,6 +11,30 @@ module Rakish
         def compileShader(src,dest,opts={})
             system("\"#{CompileShader_}\" \"#{src}\" -o \"#{dest}\"");
         end
+
+        def createShaderTasks(*files)
+            opts = (Hash === files.last) ? files.pop : {}
+            destDir = opts[:destDir];
+			if(destDir)
+                FileUtils.mkdir_p(destDir)
+            else
+                destDir = '.';
+            end
+
+			files = FileSet.new(files); # recursively expand wildcards.
+            destDir = File.expand_path(destDir);
+            tasks = [];
+            files.each do |src|
+                dest = File.basename(src);
+                dest = "#{dest.gsub(/\./,'_')}.spv";
+                dest = File.join(destDir,dest);
+                task = file dest => [ destDir, src ] do |t|
+                    compileShader(src,t.name);
+                end
+                tasks << task;
+            end
+            tasks
+        end
     end
 
     def vkt
